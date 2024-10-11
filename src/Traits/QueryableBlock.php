d08a7f8dcd1675afe99ff7eac76fc4839ee52036
wildwalks <matt@wildwalks.com>
2024-10-11 15:24:43 +1100

    Update QueryableBlock.php
    Change logic for 'in' to be more intuitive and added 'has' (similar to how 'in' was)

--- src/Traits/QueryableBlock.php
+++ src/Traits/QueryableBlock.php
@@ -55,11 +55,8 @@ trait QueryableBlock
                 '<=' => $elementToCheck->get($field) <= $value,
                 '!=' => $elementToCheck->get($field) != $value,
                 '!==' => $elementToCheck->get($field) !== $value,
-                'like' => self::like($elementToCheck->get($field), $value),
-                'in' => (is_array($elementToCheck->get($field)) && is_string($value) 
-                            ? in_array($value, $elementToCheck->get($field))
-                            : in_array($elementToCheck->get($field), $value)
-                            ),
+                'in' => in_array($value, $elementToCheck->get($field)),
+                'has' => in_array($elementToCheck->get($field), $value),
                 default => $elementToCheck->get($field) === $value,
             };
             if ($found) {
